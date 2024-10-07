ac867feda89416d710ff5146d359a959308a808c
Sergio L. Benítez D. <sergio.kun21@gmail.com>
2024-10-07 17:16:33 -0500

    hck: pass the entire response and access to the property at render level to pass the tests.

--- packages/react-stocks-data/src/components/stock-data/index.js
+++ packages/react-stocks-data/src/components/stock-data/index.js
@@ -6,7 +6,7 @@ export default function StockData() {
   const API = "https://jsonmock.hackerrank.com/api/stocks?date=";
 
   const searchDateRef = useRef();
-  const [stockData, setStockData] = useState([]);
+  const [stockData, setStockData] = useState();
 
   const handleSubmit = (event) => {
     const searchDate = searchDateRef.current.value;
@@ -15,14 +15,7 @@ export default function StockData() {
     fetch(API + searchDate)
     .then((response) => response.json())
     .then((result) => {
-      const { data } = result
-      console.log(data);
-
-      if (data.length === 0) {
-        setStockData(null);
-      } else {
-        setStockData(data[0]);
-      }
+      setStockData(result.data);
     })
   } 
 
@@ -37,18 +30,21 @@ export default function StockData() {
         </form>
       </section>
       {
-        stockData
-          ?
+        stockData && (
+          stockData.length > 0
+          ? (
             <ul className="mt-50 slide-up-fade-in styled" id="stockData" data-testid="stock-data">
-                <li className="py-10">Open: {stockData.open}</li>
-                <li className="py-10">Close: {stockData.close}</li>
-                <li className="py-10">High: {stockData.high}</li>
-                <li className="py-10">Low: {stockData.low}</li>
+              <li className="py-10">Open: {stockData[0].open}</li>
+              <li className="py-10">Close: {stockData[0].close}</li>
+              <li className="py-10">High: {stockData[0].high}</li>
+              <li className="py-10">Low: {stockData[0].low}</li>
             </ul>
-          :
+          ) : ( 
             <div className="mt-50 slide-up-fade-in" id="no-result" data-testid="no-result">
-              Result Not Found
+              No Results Found
             </div>
+          )
+        )
       }
     </div>
   );
